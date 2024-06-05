19d4171748983627c3e85b01d6982b05f59da351
Leticia Faria <leticiaemanuella96@gmail.com>
2024-06-05 09:16:19 -0300

    curso logica programacao em js

---
+++ logica-programacao/logica-js-projeto_inicial/logica-js-projeto_inicial/contador.js
@@ -0,0 +1,36 @@
+// contador de 1 até 10 ----------------------------------------------------------------------------------------------
+
+// let contador = 1;
+
+// while (contador <= 10) {
+//     alert(`Executando a iteração ${contador}`);
+//     contador++;
+// }
+
+// contador de 10 até 0 ----------------------------------------------------------------------------------------------
+
+// let contador = 10;
+
+// while (contador >= 0) {
+//     alert(`Executando a iteração ${contador}`);
+//     contador--;
+// }
+
+// contagem regressiva -----------------------------------------------------------------------------------------------
+
+// let numero = prompt('Escolha um número entre 1 e 10');
+
+// while (numero >= 0) {
+//     alert(`Contagem regressiva: ${numero}`);
+//     numero--;
+// }
+
+// contagem progressiva -----------------------------------------------------------------------------------------------
+
+let contador = 0;
+let numero = prompt('Escolha um número entre 1 e 10');
+
+while (contador <= numero) {
+    alert(`Contagem progressiva: ${contador}`);
+    contador++
+}
