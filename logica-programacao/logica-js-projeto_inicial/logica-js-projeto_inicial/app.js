3123ef8821b0318cdf8c8683c3fff9c10c671c9d
Leticia Faria <leticiaemanuella96@gmail.com>
2024-06-06 12:44:33 -0300

    execucao do desafio de boas praticas de programacao

--- logica-programacao/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-programacao/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,15 +1,15 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não foi igual ao número secreto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} tentativas`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -19,3 +19,12 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} ${palavraTentativa}.`)
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} tentativas`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto (${numeroSecreto}) com ${tentativas} tentativa`);
+// }
---
+++ logica-programacao/logica-js-projeto_inicial/logica-js-projeto_inicial/boas_vindas.js
@@ -0,0 +1,60 @@
+// nome ---------------------------------------------------------------------------------------------
+// let nome = prompt('Qual o seu nome?');
+// console.log(`Olá, ${nome}!`);
+// alert(`Olá, ${nome}!`);
+
+// linguagem de programação ------------------------------------------------------------------------
+// let linguagem = prompt("Qual a linguagem de programação que você mais gosta?");
+
+// soma --------------------------------------------------------------------------------------------
+// let valor1 = 5;
+// let valor2 = 3;
+// let resultado = valor1 + valor2;
+
+// console.log(`A soma de ${valor1} e ${valor2} é ${resultado}.`);
+
+// diferença ---------------------------------------------------------------------------------------
+// let resultado2 = valor1 - valor2;
+// console.log(`A diferença entre ${valor1} e ${valor2} é ${resultado2}.`);
+
+// maioridade --------------------------------------------------------------------------------------
+// let idade = prompt('Qual a sua idade?');
+
+// if (idade >= 18) {
+//     console.log("Você é maior de idade.");
+// } else {
+//     console.log("Você é menor de idade.");
+// }
+
+// número positivo ou negativo ---------------------------------------------------------------------
+// let numero = prompt("Escolha um número:");
+
+// if (numero < 0) {
+//     console.log("Este número é negativo.");
+// } else {
+//     if (numero == 0) {
+//         console.log("Este número é zero.");
+//     } else {
+//         console.log("Este numero é positivo.");
+//     }
+// }
+
+// números 1 a 10 -----------------------------------------------------------------------------------
+
+// let contagem = 1;
+
+// while (contagem <= 10) {
+//     alert(`Status da contagem: ${contagem}`);
+//     contagem++;
+// }
+
+// avaliação ----------------------------------------------------------------------------------------
+
+// let nota = prompt('Dê uma nota a este jogo:');
+// let avaliacao = nota >= 7 ? "aprovado! :)" : 'reprovado! :(';
+
+// alert(`Este jogo foi ${avaliacao}`);
+
+// numero aleatorio ---------------------------------------------------------------------------------
+
+console.log(parseInt(Math.random() * 1000 + 1));
