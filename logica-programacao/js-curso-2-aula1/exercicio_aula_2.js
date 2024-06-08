db5acd6eb756341fedc4dca5cfe845d0ea6f5950
Leticia Faria <leticiaemanuella96@gmail.com>
2024-06-08 15:23:02 -0300

    finalizacao aula 2 curso 2 logica de programacao

---
+++ logica-programacao/js-curso-2-aula1/exercicio_aula_2.js
@@ -0,0 +1,60 @@
+// função olá mundo ------------------------------------------------------------------------
+
+// function olaMundo() {
+//     console.log("Olá mundo!")
+// }
+
+// olaMundo()
+
+
+// função olá fulano ------------------------------------------------------------------------
+
+// let nome = prompt("Qual o seu nome?")
+
+// function olaFulano(nome) {
+//     alert(`Olá, ${nome}!`)
+// }
+
+// olaFulano(nome = nome)
+
+// função dobro -----------------------------------------------------------------------------
+
+// let numero = prompt("Fale um número:")
+
+
+// function dobro(numero) {
+//     let dobro = parseInt(numero) * 2
+//     alert(`O dobro de ${numero} é ${dobro}.`)
+// }
+
+// dobro(numero = numero)
+
+// função média ------------------------------------------------------------------------------
+
+// let numero1 = prompt("Fale um número:")
+// let numero2 = prompt("Fale outro número:")
+// let numero3 = prompt("Fale mais um número:")
+
+// function media(numero1, numero2, numero3) {
+//     let media = (parseInt(numero1) + parseInt(numero2) + parseInt(numero3)) / 3
+//     alert(`A média dos 3 números informados é ${media}.`)
+// }
+
+// media(numero1, numero2, numero3)
+
+// função maior ------------------------------------------------------------------------------
+
+// let numero1 = prompt("Fale um número:")
+// let numero2 = prompt("Fale outro número:")
+
+// function maior(numero1, numero2) {
+//     if (numero1 > numero2) {
+//         alert(`O maior número é ${numero1}.`)
+//     } else if (numero1 == numero2) {
+//         alert(`Os dois números informados são iguais.`)
+//     } else {
+//         alert(`O maior número é ${numero2}`)
+//     }
+// }
+
+// maior(numero1, numero2)
