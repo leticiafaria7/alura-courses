e5c04f36df1c10ffba14ca3cf06140e3ce8e6983
Leticia Faria <leticiaemanuella96@gmail.com>
2024-06-09 15:15:28 -0300

    finalizacao aula 3

--- logica-programacao/js-curso-2-aula1/exercicio_aula_2.js
+++ logica-programacao/js-curso-2-aula1/exercicio_aula_2.js
@@ -58,3 +58,101 @@
 // }
 
 // maior(numero1, numero2)
+
+// IMC ----------------------------------------------------------------------------------------
+
+// let altura = prompt('Qual a sua altura (em cm)?');
+// let peso = prompt('Qual é o seu peso (em kg)?');
+
+// function textoAvaliacao(imc) {
+//     if (imc < 18) {
+//         avaliacao = "abaixo do ideal";
+//     } else if (imc <= 25) {
+//         avaliacao = "dentro do ideal";
+//     } else if (imc <= 30) {
+//         avaliacao = "alto";
+//     } else {
+//         avaliacao = "você está com obesidade";
+//     }
+
+//     return avaliacao
+// }
+
+// function imc(altura, peso) {
+//     let imc = peso / ((altura / 100) ** 2);
+//     alert(`Seu IMC é ${parseInt(imc)} (${textoAvaliacao(imc)})`);
+// }
+
+// imc(altura, peso)
+
+// fatorial ------------------------------------------------------------------------------------
+
+// let numero = prompt("Diga um número para ter seu fatorial calculado:");
+
+// function calcularFatorial(numero) {
+//     let numeroInicial = numero;
+//     let fatorial = numero;
+//     while (numero > 1) {
+//         numero--;
+//         fatorial = fatorial * (numero);
+//     }
+//     // return(fatorial)
+//     alert(`O fatorial de ${numeroInicial} é ${fatorial}.`)
+// }
+
+// calcularFatorial(numero);
+
+// converter dólar ------------------------------------------------------------------------------------
+// let valor = prompt('Diga um valor em reais para ser convertido em dólar (use "." como separador de centavos):');
+
+// function converterDolar(valor) {
+//     dolar = (valor * 4.8).toFixed(2);
+//     alert(`R$ ${valor} são US$ ${dolar}`);
+// }
+
+// converterDolar(valor);
+
+// perimetro -----------------------------------------------------------------------------------------
+
+// let largura = prompt("Qual a largura da sala (em metros)?")
+// let comprimento = prompt("Qual o comprimento da sala (em metros)?")
+
+// function exibirTextoNaTela(tag, texto) {
+//     let campo = document.querySelector(tag);
+//     campo.innerHTML = texto;
+// }
+
+// function calcularArea(largura, comprimento) {
+//     let area = largura * comprimento;
+//     return area;
+// }
+
+// function calcularPerimetro(largura, comprimento) {
+//     let perimetro = (largura * 2) + (comprimento * 2);
+//     return perimetro;
+// }
+
+// let area = calcularArea(largura, comprimento)
+// let perimetro = calcularPerimetro(largura, comprimento)
+
+// alert(`A área da sala é ${area} m² e o seu perímetro é ${perimetro} m.`)
+
+// exibirTextoNaTela('h1', 'Calcular perímetro da sala')
+// exibirTextoNaTela('p', `A área da sala é ${area} e o seu perímetro é ${perimetro}.`)
+
+
+// tabuada --------------------------------------------------------------------------------------------
+let numero = prompt("Diga um número");
+
+function mostrarTabuada(numero) {
+    for (let i = 1; i <= 10; i++) {
+        let resultado = numero * i;
+        console.log(`${numero} x ${i} = ${resultado}`);
+    }
+}
+
+// Exemplo de uso
+mostrarTabuada(numero);
+
+
+
