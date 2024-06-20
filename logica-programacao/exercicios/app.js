813333a37dea26e6be5903948f2304c6737bd6bc
Leticia Faria <leticiaemanuella96@gmail.com>
2024-06-20 20:15:18 -0300

    programa conversor de temperatura

--- logica-programacao/exercicios/app.js
+++ logica-programacao/exercicios/app.js
@@ -6,3 +6,21 @@ function selecionar() {
     document.getElementById('opcao-selecionada').textContent = produto;
 }
 
+// converter temperatura
+function converter() {
+    let unidadeInicial = document.getElementById('tipo-unidade').value;
+    let temperaturaInicial = document.getElementById('temperatura').value;
+
+    if (unidadeInicial == 'celsius') {
+        temperaturaConvertida = parseInt(temperaturaInicial * (9/5) + 32);
+        unidadeFinal = 'fahrenheit';
+    } else {
+        temperaturaConvertida = parseInt((temperaturaInicial - 32) * (5/9));
+        unidadeFinal = 'celsius';
+    }
+    
+    let textoOutput = `${temperaturaInicial} graus ${unidadeInicial} equivalem a ${temperaturaConvertida} graus ${unidadeFinal}`
+    document.getElementById('output').innerHTML = textoOutput;
+}
+
+// (0 °C × 9/5) + 32 = 32 °F
