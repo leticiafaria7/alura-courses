1eb3fc8c2f4b1b2ccf5415cb91d5cabe1d89f1c0
Leticia Faria <leticiaemanuella96@gmail.com>
2024-06-21 11:18:05 -0300

    projeto amigo secreto

---
+++ logica-programacao/3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -0,0 +1,50 @@
+let listaAmigos = [];
+let listaDuplas = [];
+let numerosSorteados = [];
+
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo').value;
+
+    if (listaAmigos.includes(amigo)) {
+        alert(`${amigo} já está na lista!`);
+        document.getElementById('nome-amigo').value = '';
+    } else if (amigo == '') {
+        alert(`Inclua o nome de um amigo`);
+    } else {
+        listaAmigos.push(amigo);
+        document.getElementById('lista-amigos').innerHTML = listaAmigos;
+        document.getElementById('nome-amigo').value = '';
+    }
+}
+
+function sortear() {
+    if (listaAmigos.length < 2) {
+        alert('A lista de amigos deve ter pelo menos 2 amigos!');
+    } else {
+        listaAmigos.forEach((amigo, indexAmigo) => {
+            let numeroSorteado;
+    
+            do {
+                numeroSorteado = Math.floor(Math.random() * listaAmigos.length);
+            } while (numeroSorteado == indexAmigo || numerosSorteados.includes(numeroSorteado));
+    
+            numerosSorteados.push(numeroSorteado);
+    
+            let amigoSecreto = listaAmigos[numeroSorteado];
+            let dupla = `${amigo} → ${amigoSecreto}<br>`;
+            listaDuplas.push(dupla);
+            console.log(listaDuplas);
+        })
+    
+        document.getElementById('lista-sorteio').innerHTML = listaDuplas.join('');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('lista-amigos').innerHTML = [];
+    document.getElementById('nome-amigo').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+    listaAmigos = [];
+    listaDuplas = [];
+    numerosSorteados = [];
+}
--- logica-programacao/exercicios/app.js
+++ logica-programacao/exercicios/app.js
@@ -23,4 +23,44 @@ function converter() {
     document.getElementById('output').innerHTML = textoOutput;
 }
 
-// (0 °C × 9/5) + 32 = 32 °F
+// concatenar listas
+let lista1 = [1, 2, 3];
+let lista2 = [4, 5, 6];
+let novaLista = lista1.concat(lista2);
+novaLista.pop();
+// console.log(novaLista);
+
+// algoritmo fisher yates
+const vogais = ['a', 'e', 'i', 'o', 'u'];
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        const elemento = lista[indice - 1];
+        lista[indice - 1] = lista[indiceAleatorio];
+        lista[indiceAleatorio] = elemento;
+    }
+}
+
+// console.log(embaralha(vogais));
+embaralha(novaLista);
+console.log(novaLista);
+
+
+// remover duplicatas
+function removerDuplicatas(lista) {
+    listaSemDuplicatas = [];
+    lista.forEach((elemento) => {
+        if(!listaSemDuplicatas.includes(elemento)) {
+            listaSemDuplicatas.push(elemento);
+        }
+    })
+
+    return listaSemDuplicatas;
+}
+
+let listaTeste = ["RJ", "MG", "SP", "SC", "SP", "SP", "PR", "PE", "PA", "RJ", "MG"];
+
+console.log(listaTeste);
+console.log(removerDuplicatas(listaTeste));
