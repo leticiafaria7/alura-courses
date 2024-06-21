ece75649e22c07cd811be204a157b5e201dc4e14
Leticia Faria <leticiaemanuella96@gmail.com>
2024-06-21 18:04:55 -0300

    insercao de listas e lacos de repeticao no app de cadastro de restaurantes

--- python-oop/sabor-express/app.py
+++ python-oop/sabor-express/app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Ninita', 'Topo do Mundo']
+
 def exibir_nome_do_programa():
     print("Sabor Express\n")
 
@@ -10,22 +12,55 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
+    exibir_subtitulo('Finalizando o app')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal: ')
+    main()
+
+def exibir_subtitulo(subtitulo):
     os.system('cls') # limpa o terminal
-    print('Finalizando o app')
+    print(f'{subtitulo}\n')
+
+def opcao_invalida():
+    print('Opção inválida!')
+    voltar_ao_menu_principal()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f"O restaurante {nome_do_restaurante} foi cadastrado com sucesso!")
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Restaurantes cadastrados:')
+    
+    for restaurante in restaurantes:
+        print(f'. {restaurante}')
+
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restarurante')
-    else:
-        finalizar_app()
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Ativar restarurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
--- python-oop/sabor-express/exercicios.py
+++ python-oop/sabor-express/exercicios.py
@@ -20,17 +20,52 @@
 #         print('Você é um adulto')
 
 # verificar senha ---------------------------------------------------------------------------------
-from pwinput import pwinput
+# from pwinput import pwinput
 
-usuario = 'leticia123'
-senha = '@literalbadge'
+# usuario = 'leticia123'
+# senha = '@literalbadge'
 
-usuario_fornecido = input('Usuário: ')
-senha_fornecida = pwinput(prompt = "Senha: ")
+# usuario_fornecido = input('Usuário: ')
+# senha_fornecida = pwinput(prompt = "Senha: ")
 
-if usuario_fornecido != usuario:
-    print('O usuário fornecido não existe no sistema.')
-elif senha_fornecida != senha:
-    print('Senha incorreta')
-else:
-    print('Acesso concedido')
+# if usuario_fornecido != usuario:
+#     print('O usuário fornecido não existe no sistema.')
+# elif senha_fornecida != senha:
+#     print('Senha incorreta')
+# else:
+#     print('Acesso concedido')
+
+# soma dos números ímpares de 1 a 10 --------------------------------------------------------------
+# soma = 0
+# for _ in range(1, 11):
+#     if _ % 2 != 0:
+#         soma = soma + _
+    
+# print(soma)
+
+# números de 1 a 10 de forma decrescente ----------------------------------------------------------
+# lista = range(10)
+# for idx, _ in enumerate(lista):
+#     print(len(lista) - idx)
+
+# tabuada -----------------------------------------------------------------------------------------
+# numero = int(input('Digite um número: '))
+# print(f'\nTabuada do número {numero}:')
+# for _ in range(10):
+#     print(numero * (_ + 1))
+
+# média dos valores da lista ----------------------------------------------------------------------
+def criar_lista_numeros():
+    numeros = input('Digite uma lista de números separados por vírgula: ')
+    lista = numeros.replace(' ', '').split(',')
+    lista = [int(numero) for numero in lista]
+    return lista
+
+while True:
+    try:
+        lista = criar_lista_numeros()
+        break
+    except:
+        print('Um dos números não é válido! Por favor, tente novamente.\n')
+
+print(f'A média dos números informados é {sum(lista) / len(lista)}')
