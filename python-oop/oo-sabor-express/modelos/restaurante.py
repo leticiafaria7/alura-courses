f845c069ac879ce4ae99ad091df5be8e607e20b7
Leticia Faria <leticiaemanuella96@gmail.com>
2024-06-24 20:41:19 -0300

    criando uma classe e instanciando objetos com python

---
+++ python-oop/oo-sabor-express/modelos/restaurante.py
@@ -0,0 +1,20 @@
+class Restaurante:
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+Restaurante.listar_restaurantes()
